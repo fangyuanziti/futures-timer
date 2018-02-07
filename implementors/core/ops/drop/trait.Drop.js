2778cf8fababea19b67bbee08e2393e076d93128
Travis CI User <travis@example.org>
2018-02-07 17:29:09 +0000

    Documentation for alexcrichton/futures-timer@4637e67

---
+++ implementors/core/ops/drop/trait.Drop.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["futures_timer"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"futures_timer/struct.Delay.html\" title=\"struct futures_timer::Delay\">Delay</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"futures_timer/struct.Timer.html\" title=\"struct futures_timer::Timer\">Timer</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
